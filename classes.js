432d70a980c176b7e6e82a958bf3745cc89d623b
成田 英樹 <naritaenator@gmail.com>
2011-05-08 02:19:23 +0900

    JSBuilder3 で build させる

---
+++ classes.js
@@ -0,0 +1,5 @@
+/*
+Copyright(c) 2011 NRT Engineer.
+licensing@nrt-engineer.biz
+*/
+Ext.define("NRT.calendar.AbstractPanel",{extend:"Ext.panel.Panel"});Ext.define("NRT.calendar.Panel",{extend:"NRT.calendar.AbstractPanel",alias:"widget.calendar"});
