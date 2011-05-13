5c5624b7c350a90123bee09611ef5c2eff90c8e6
成田 英樹 <naritaenator@gmail.com>
2011-05-13 12:18:31 +0900

    add DayViewBody DayViewHeader

---
+++ js/ux/Nrt/calendar/templates/DayViewBodyTemplate.js
@@ -0,0 +1,47 @@
+/**
+ * {{{ Nrt.calendar.templates.DayViewTemplate
+ *
+ */
+Ext.define( 'Nrt.calendar.templates.DayViewBodyTemplate', {
+	  extend:				'Nrt.calendar.templates.AbstractViewTemplate'
+
+	, template:				[
+		  '<table class="nrt-cal-bg-tbl" cellspacing="0" cellpadding="0">'
+		, 	'<tbody>'
+		, 		'<tr height="1">'
+		, 			'<td class="nrt-cal-gutter"></td>'
+		, 			'<td colspan="{dayCount}">'
+		, 				'<div class="nrt-cal-bg-rows">'
+		, 					'<div class="nrt-cal-bg-rows-inner">'
+		, 						'<tpl for="times">'
+		, 							'<div class="nrt-cal-bg-row">'
+		, 								'<div class="nrt-cal-bg-row-div ext-row-{#}"></div>'
+		, 							'</div>'
+		, 						'</tpl>'
+		, 					'</div>'
+		, 				'</div>'
+		, 			'</td>'
+		, 		'</tr>'
+		, 		'<tr>'
+		, 			'<td class="nrt-cal-day-times">'
+		, 				'<tpl for="times">'
+		, 					'<div class="nrt-cal-bg-row">'
+		, 						'<div class="nrt-cal-day-time-inner">{.}</div>'
+		, 					'</div>'
+		, 				'</tpl>'
+		, 			'</td>'
+		, 			'<tpl for="days">'
+		, 				'<td class="nrt-cal-day-col">'
+		, 					'<div class="nrt-cal-day-col-inner">'
+		, 						'<div id="{[this.id]}-day-col-{.:date("Ymd")}" class="nrt-cal-day-col-gutter"></div>'
+		, 					'</div>'
+		, 				'</td>'
+		, 			'</tpl>'
+		, 		'</tr>'
+		, 	'</tbody>'
+		, '</table>'
+	]
+
+});
+// }}}
+// vim: foldmethod=maker commentstring=%s*%s : // tabstop=4 shiftwidth=4 autoindent
